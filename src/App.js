58aae2ed2bee172f31f6170f74e44447927fe9fc
kreativcare <109919243+kreativcare@users.noreply.github.com>
2023-09-24 20:20:37 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>hello</h1>
+      </header>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+    </div>
+  );
+}
