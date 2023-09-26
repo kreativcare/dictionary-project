ef8e69d40b06b76ce9083061480a8c946fa8ff12
kreativcare <109919243+kreativcare@users.noreply.github.com>
2023-09-26 20:56:07 +0200

    added axios and api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
@@ -6,8 +7,15 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    function handleResponse(response) {
+      console.log(response.data.meanings[0]);
+    }
+
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=17c4t0bff12cb4a64a5e588oa39474f2`;
+    axios.get(apiUrl).then(handleResponse);
   }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
