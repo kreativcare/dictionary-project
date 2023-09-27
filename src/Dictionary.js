ab268ad4319dd9bf6e288a06dd3037a7d1036c8e
kreativcare <109919243+kreativcare@users.noreply.github.com>
2023-09-27 21:31:57 +0200

    added rand rendered result and meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function search(event) {
     event.preventDefault();
 
     function handleResponse(response) {
       console.log(response.data);
+      console.log(response.data.meanings[0].definition);
+      setResults(response.data);
     }
 
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=17c4t0bff12cb4a64a5e588oa39474f2`;
@@ -25,6 +29,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
