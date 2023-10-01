0d20f7d834f23f9c0112e16589bbc8e4e62cf7de
kreativcare <109919243+kreativcare@users.noreply.github.com>
2023-10-01 15:13:32 +0200

    added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,15 +1,23 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>
-        {props.meaning.definition}
-        <br />
-        <em>{props.meaning.example}</em>
-      </p>
+      <div>
+        <p>
+          <strong>Definition: </strong>
+          {props.meaning.definition}
+          <br />
+          <strong>Example: </strong>
+          <em>{props.meaning.example}</em>
+          <br />
+          <strong>Synonym: </strong>
+          <Synonyms synonyms={props.meaning.synonyms} />
+        </p>
+      </div>
     </div>
   );
 }
